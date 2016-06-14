b6e90d5358ef88f4ae4a5c86e70b9511067d337a
Jhon Marco Achata Limachi <jmal_444@hotmail.com>
2016-06-14 07:51:02 -0500

    Modificaciones para el SEO y algunos arreglos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 $(document).on('ready', main);
-
+$(window).scroll(function(){
+    if($(this).scrollTop() > 0 ){
+        $( "header" ).animate({
+            color: "green",
+            backgroundColor: "rgb( 20, 20, 20 )"
+          });
+        $('header').addClass('header-border');
+    }else {
+        $('header').removeClass('header-border');
+    }
+});
 function main(){
-	$('header li a').on('click', goTo);
-	$('#view-more-a').on('click', goTo);
+    $('header li a').on('click', goTo);
+    $('#view-more-a').on('click', goTo);
 }
 function goTo(){
-	var section = $(this).attr('href');
-	$('body, html').animate({
-		scrollTop: $(section).offset().top-100
-	}, 500);
+    var section = $(this).attr('href');
+    $('body, html').animate({
+        scrollTop: $(section).offset().top-100
+    }, 500);
 
-	return false;
+    return false;
 }
-
 $('.skillbar').each(function(){
-	$(this).find('.skillbar-bar').animate({width:$(this).attr('data-percent')
-	},2000);
+    $(this).find('.skillbar-bar').animate({width:$(this).attr('data-percent')
+    },2000);
 });
